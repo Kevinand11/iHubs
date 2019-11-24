3e9f98ab4b9c65e46757e931deca479245a0811c
Izuchukwu Kevin <kevinfizu@gmail.com>
2019-11-24 04:10:41 +0100

    Deleting isLoggedOut Middleware

--- routes/api/.js
+++ routes/api/.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
 let indexController = require("../../app/controllers/api/index");
-let { isLoggedIn, isLoggedOut } = require("../../app/middlewares");
+let { isLoggedIn } = require("../../app/middlewares");
 
 router.get("/profile", isLoggedIn, indexController.profile);
-router.post("/login", isLoggedOut, indexController.login);
-router.post("/register", isLoggedOut, indexController.register);
+router.post("/login", indexController.login);
+router.post("/register", indexController.register);
 router.get("/([A-zd/_.\\s]+)?", indexController.error);
 
 module.exports = router;
